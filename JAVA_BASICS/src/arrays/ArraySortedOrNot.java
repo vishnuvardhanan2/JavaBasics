cebf37d611aeb97273b439a2f14c22346c3f58f2
vishnuvardhanan <vishnuvardhanan2@gmail.com>
2026-03-17 12:24:01 +0530

    committing change in array sorted class

--- JAVA_BASICS/src/arrays/ArraySortedOrNot.java
+++ JAVA_BASICS/src/arrays/ArraySortedOrNot.java
@@ -12,9 +12,9 @@ public class ArraySortedOrNot {
 		return true;
 	}
 	public static void main(String[] args) {
-		int[] ar= {1,2,3,4,5,6};
-		int n= ar.length;
-		System.out.println(calc(ar,n));
+		int[] ar= {1,2,3,4,5,7};
+		int m= ar.length;
+		System.out.println(calc(ar,m));
 	}
 
 }
